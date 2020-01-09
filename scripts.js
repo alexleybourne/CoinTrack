562831ae4d2e51a3638b664b0231a40814d7cb48
Alex Leybourne <alexleybourne1@gmail.com>
2020-01-09 16:38:24 +1000

    Added anime JS

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
-// Put all your Javascript Goodness in here
+
+function pageLoaded() {
+
+    console.log('Yeet')
+
+    let animation = anime({
+        targets: '.animeTest',
+        translateX: 100,
+        borderRadius: 50,
+        duration: 2000,
+        easing: 'linear',
+        direction: 'alternate',
+        loop: 'infinte'
+      });    
+
+}
