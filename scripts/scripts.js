5a40b13cf28fe439ce3e07a5c38c00130b82420c
Alex Leybourne <alexleybourne1@gmail.com>
2020-01-10 15:12:34 +1000

    Resume Bit Done

---
+++ scripts/scripts.js
@@ -0,0 +1,15 @@
+
+function pageLoaded() {
+
+
+  console.log('Yeet')
+
+  let animation = anime({
+    targets: '.spinAnimation',
+    rotate: '360deg',
+    duration: 2000,
+    easing: 'linear',
+    loop: true
+    }) 
+
+}
