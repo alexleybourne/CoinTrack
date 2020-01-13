c87747c4be0b16406c458cea693fc9b4732e3226
Alex Leybourne <alexleybourne1@gmail.com>
2020-01-13 11:29:03 +1000

    Animation in and out and Nav created

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,18 +1,60 @@
 
 function pageLoaded() {
 
-  Sticker.init('.sticker')
-  
-  console.log('Yeet')
+  let animation1 = anime({
+    targets: '.alexLetters',
+    rotate: -90,
+    duration: 0
+    }) 
 
   let animation = anime({
     targets: '.alexLetters',
     opacity: 1,
-    height: 200,
-    width: 200,
+    height: "18vw",
     rotate: 0,
     delay: anime.stagger(200),
-    duration: 2000
+    duration: 1800
     }) 
 
 }
+
+let scrollPosition = 0
+
+window.addEventListener('scroll', function() {
+  scrollPosition = window.pageYOffset
+
+  if (scrollPosition > 200) {
+    navAnimation()
+  }
+
+  if (scrollPosition < 200) {
+
+    let animation = anime({
+      targets: '.alexLetters',
+      opacity: 1,
+      height: "18vw",
+      rotate: 0,
+      delay: anime.stagger(200),
+      duration: 1800
+    }) 
+
+  }
+
+})
+
+
+function navAnimation() {
+
+  console.log('nav animation')
+  
+  let animation = anime({
+    targets: '.alexLetters',
+    opacity: 0,
+    height: "0vw",
+    rotate: 0,
+    duration: 1000
+  })
+  
+  
+}
+
