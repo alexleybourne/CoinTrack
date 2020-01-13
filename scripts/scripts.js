d4a98c8c925d6c960fe3488f6368005d4015fa38
Alex Leybourne <alexleybourne1@gmail.com>
2020-01-13 12:50:17 +1000

    Nav in and Out getting htere

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,35 +1,65 @@
+// Page load and setup
 
 function pageLoaded() {
 
-  let animation1 = anime({
+  let alexTextinAnimation1 = anime({
     targets: '.alexLetters',
     rotate: -90,
     duration: 0
-    }) 
+  }) 
 
-  let animation = anime({
+  let alexTextinAnimation = anime({
     targets: '.alexLetters',
     opacity: 1,
     height: "18vw",
     rotate: 0,
     delay: anime.stagger(200),
     duration: 1800
-    }) 
+  }) 
 
+  let navTextAnimation = anime({
+    targets: '.navLetters',
+    translateY: "-100px",
+    duration: 0
+  }) 
 }
 
+// Scroll Value
 let scrollPosition = 0
+let landingHeight = 0
 
 window.addEventListener('scroll', function() {
   scrollPosition = window.pageYOffset
+  landingHeight = document.getElementById('landingArea').offsetHeight
+  landingHeight = landingHeight / 2
 
-  if (scrollPosition > 200) {
-    navAnimation()
+  // Bring In Animations
+  if (scrollPosition > landingHeight) {
+    let hideAlexTextinAnimation = anime({
+      targets: '.alexLetters',
+      opacity: 0,
+      height: "0vw",
+      rotate: 0,
+      duration: 1000
+    })
+  
+    let navAnimation = anime({
+      targets: '.navLetters',
+      opacity: 1,
+      translateY: 0,
+      delay: anime.stagger(100),
+      duration: 1000
+    }) 
+
+    document.getElementById("navBar").classList.remove("navBarHidden")
+    document.getElementById("navBar").classList.add("navBar")
+
   }
 
-  if (scrollPosition < 200) {
+  // Hide Animations
+  if (scrollPosition < landingHeight) {
 
-    let animation = anime({
+    let alexTextinAnimation = anime({
       targets: '.alexLetters',
       opacity: 1,
       height: "18vw",
@@ -38,23 +68,20 @@ window.addEventListener('scroll', function() {
       duration: 1800
     }) 
 
+    let navTextAnimation = anime({
+      targets: '.navLetters',
+      translateY: "-100px",
+      opacity: 0,
+      duration: 0
+    })
+
+    document.getElementById("navBar").classList.remove("navBar")
+    document.getElementById("navBar").classList.add("navBarHidden")
+
   }
 
 })
 
 
-function navAnimation() {
 
-  console.log('nav animation')
-  
-  let animation = anime({
-    targets: '.alexLetters',
-    opacity: 0,
-    height: "0vw",
-    rotate: 0,
-    duration: 1000
-  })
-  
-  
-}
 
