50a40ee6f49326836e0e44cf70fdb51097340e39
alexleybourne <alexleybourne1@gmail.com>
2020-03-17 20:12:24 +1000

    Setup

--- portfolio/src/views/Home.vue
+++ portfolio/src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <div data-tilt data-tilt-full-page-listening>
+        <img src="https://static.wixstatic.com/media/2cd43b_5f099d303fb742dfb10d3892386e0eb6~mv2.png/v1/fill/w_320,h_322,fp_0.50_0.50/2cd43b_5f099d303fb742dfb10d3892386e0eb6~mv2.png" alt="meow">
+      </div>
+    </div>
   </div>
 </template>
 
