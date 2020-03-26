639456093540d6f09625e17ae133c6e0c4a6a092
alexleybourne <alexleybourne1@gmail.com>
2020-03-26 22:36:20 +1000

    Skills icons spacing css

--- portfolio/src/views/Home.vue
+++ portfolio/src/views/Home.vue
@@ -15,49 +15,67 @@
         <p :class="{ whiteText: DarkMode }">I'm a young creative-minded Developer.</p>
 
         <div class="languageIcons">
-          <div class="languageIconHolder tooltip" :class="{ whiteCard: !DarkMode, blackCard: DarkMode }">
-            <img src="@/assets/icons/languages/VueJsLogo.svg" alt="Vue Logo" class="languageIcon jello" :class="{ invert: DarkMode }" style="transform: translateY(7px)">
-            <span class="tooltiptext" :class="{ invert: DarkMode }" >Vue</span>
+          <div class="languageIconSpacer">
+            <div class="languageIconHolder tooltip" :class="{ whiteCard: !DarkMode, blackCard: DarkMode }">
+              <img src="@/assets/icons/languages/VueJsLogo.svg" alt="Vue Logo" class="languageIcon jello" :class="{ invert: DarkMode }" style="transform: translateY(7px)">
+              <span class="tooltiptext" :class="{ invert: DarkMode }" >Vue</span>
+            </div>
           </div>
         
-          <div class="languageIconHolder tooltip" :class="{ whiteCard: !DarkMode, blackCard: DarkMode }">
-            <img src="@/assets/icons/languages/ReactLogo.svg" alt="React Logo" class="languageIcon react" :class="{ invert: DarkMode }">
-            <span class="tooltiptext" :class="{ invert: DarkMode }" >React</span>
+          <div class="languageIconSpacer">
+            <div class="languageIconHolder tooltip" :class="{ whiteCard: !DarkMode, blackCard: DarkMode }">
+              <img src="@/assets/icons/languages/ReactLogo.svg" alt="React Logo" class="languageIcon react" :class="{ invert: DarkMode }">
+              <span class="tooltiptext" :class="{ invert: DarkMode }" >React</span>
+            </div>
           </div>
 
-          <div class="languageIconHolder tooltip" :class="{ whiteCard: !DarkMode, blackCard: DarkMode }">
-            <img src="@/assets/icons/languages/JavascriptLogo.svg" alt="Javascript Logo" class="languageIcon jello" :class="{ invert: DarkMode }">
-            <span class="tooltiptext" :class="{ invert: DarkMode }" >Javascript</span>
+          <div class="languageIconSpacer">
+            <div class="languageIconHolder tooltip" :class="{ whiteCard: !DarkMode, blackCard: DarkMode }">
+              <img src="@/assets/icons/languages/JavascriptLogo.svg" alt="Javascript Logo" class="languageIcon jello" :class="{ invert: DarkMode }">
+              <span class="tooltiptext" :class="{ invert: DarkMode }" >Javascript</span>
+            </div>
           </div>
 
-          <div class="languageIconHolder tooltip" :class="{ whiteCard: !DarkMode, blackCard: DarkMode }">
-            <img src="@/assets/icons/languages/NodeJsLogo.svg" alt="Node Js Logo" class="languageIcon jello" :class="{ invert: DarkMode }">
-            <span class="tooltiptext" :class="{ invert: DarkMode }" >Node Js</span>
+          <div class="languageIconSpacer">
+            <div class="languageIconHolder tooltip" :class="{ whiteCard: !DarkMode, blackCard: DarkMode }">
+              <img src="@/assets/icons/languages/NodeJsLogo.svg" alt="Node Js Logo" class="languageIcon jello" :class="{ invert: DarkMode }">
+              <span class="tooltiptext" :class="{ invert: DarkMode }" >Node Js</span>
+            </div>
           </div>
 
-          <div class="languageIconHolder tooltip" :class="{ whiteCard: !DarkMode, blackCard: DarkMode }">
-            <img src="@/assets/icons/languages/RubyLogo.svg" alt="Ruby Logo" class="languageIcon jello" :class="{ invert: DarkMode }">
-            <span class="tooltiptext" :class="{ invert: DarkMode }" >Ruby</span>
+          <div class="languageIconSpacer">
+            <div class="languageIconHolder tooltip" :class="{ whiteCard: !DarkMode, blackCard: DarkMode }">
+              <img src="@/assets/icons/languages/RubyLogo.svg" alt="Ruby Logo" class="languageIcon jello" :class="{ invert: DarkMode }">
+              <span class="tooltiptext" :class="{ invert: DarkMode }" >Ruby</span>
+            </div>
           </div>
 
-          <div class="languageIconHolder tooltip" :class="{ whiteCard: !DarkMode, blackCard: DarkMode }">
-            <img src="@/assets/icons/languages/Html5Logo.svg" alt="Html Logo" class="languageIcon jello" :class="{ invert: DarkMode }">
-            <span class="tooltiptext" :class="{ invert: DarkMode }" >Html</span>
+          <div class="languageIconSpacer">
+            <div class="languageIconHolder tooltip" :class="{ whiteCard: !DarkMode, blackCard: DarkMode }">
+              <img src="@/assets/icons/languages/Html5Logo.svg" alt="Html Logo" class="languageIcon jello" :class="{ invert: DarkMode }">
+              <span class="tooltiptext" :class="{ invert: DarkMode }" >Html</span>
+            </div>
           </div>
 
-          <div class="languageIconHolder tooltip" :class="{ whiteCard: !DarkMode, blackCard: DarkMode }">
-            <img src="@/assets/icons/languages/CSS3Logo.svg" alt="Css Logo" class="languageIcon jello" :class="{ invert: DarkMode }">
-            <span class="tooltiptext" :class="{ invert: DarkMode }" >CSS</span>
+          <div class="languageIconSpacer">
+            <div class="languageIconHolder tooltip" :class="{ whiteCard: !DarkMode, blackCard: DarkMode }">
+              <img src="@/assets/icons/languages/CSS3Logo.svg" alt="Css Logo" class="languageIcon jello" :class="{ invert: DarkMode }">
+              <span class="tooltiptext" :class="{ invert: DarkMode }" >CSS</span>
+            </div>
           </div>
 
-          <div class="languageIconHolder tooltip" :class="{ whiteCard: !DarkMode, blackCard: DarkMode }">
-            <img src="@/assets/icons/languages/SassLogo.svg" alt="Sass Logo" class="languageIcon jello" :class="{ invert: DarkMode }">
-            <span class="tooltiptext" :class="{ invert: DarkMode }" >Sass</span>
+          <div class="languageIconSpacer">
+            <div class="languageIconHolder tooltip" :class="{ whiteCard: !DarkMode, blackCard: DarkMode }">
+              <img src="@/assets/icons/languages/SassLogo.svg" alt="Sass Logo" class="languageIcon jello" :class="{ invert: DarkMode }">
+              <span class="tooltiptext" :class="{ invert: DarkMode }" >Sass</span>
+            </div>
           </div>
 
-          <div class="languageIconHolder tooltip" :class="{ whiteCard: !DarkMode, blackCard: DarkMode }">
-            <img src="@/assets/icons/languages/FireBaseLogo.svg" alt="Firebase Logo" class="languageIcon jello" :class="{ invert: DarkMode }">
-            <span class="tooltiptext" :class="{ invert: DarkMode }" >Firebase</span>
+          <div class="languageIconSpacer">
+            <div class="languageIconHolder tooltip" :class="{ whiteCard: !DarkMode, blackCard: DarkMode }">
+              <img src="@/assets/icons/languages/FireBaseLogo.svg" alt="Firebase Logo" class="languageIcon jello" :class="{ invert: DarkMode }">
+              <span class="tooltiptext" :class="{ invert: DarkMode }" >Firebase</span>
+            </div>
           </div>
         </div>
        
