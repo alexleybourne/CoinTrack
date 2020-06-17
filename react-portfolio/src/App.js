aa23f8657651cb8201743bbed6036e8d91a8b1d3
alexleybourne <alexleybourne1@gmail.com>
2020-06-17 22:47:14 +1000

    Some Scss goodness

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -6,7 +6,7 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className="hero">
       <div className="container">
         <div className="hero-inner">
           <div className="hero-content">
