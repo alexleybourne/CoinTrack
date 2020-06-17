1ddfa347d1c67f9cdcf5bcbdc9931607a6beedfd
alexleybourne <alexleybourne1@gmail.com>
2020-06-17 21:48:38 +1000

    divs planned out

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -4,7 +4,26 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <h1>Portfolio</h1>
+      <div className="container">
+        <div className="hero-inner">
+          <div className="hero-content">
+            <div className="hero-content-inner">
+              <h1>
+                <div className="hero-content-line">
+                  <div className="hero-content-line-inner">Hello</div>
+                </div>
+                <div className="hero-content-line">
+                  <div className="hero-content-line-inner">I'm Alex Leybourne</div>
+                </div>
+                <div className="hero-content-line">
+                  <div className="hero-content-line-inner">Full Stack dev</div>
+                </div>
+              </h1>
+            </div>
+          </div>
+          <div className="hero-images"></div>
+        </div>
+      </div>
     </div>
   );
 }
