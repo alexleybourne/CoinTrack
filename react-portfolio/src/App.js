c47fe9b2e553c1beec5262e409ce87bc6da9fc4b
alexleybourne <alexleybourne1@gmail.com>
2020-06-18 00:09:32 +1000

    All text now animated

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -14,7 +14,7 @@ function App() {
   let app = useRef(null)
   let images = useRef(null)
   let content = useRef(null)
-  let tl  = new TimelineLite()
+  let tl  = new TimelineLite({delay: .8})
 
   useEffect(() => {
     // Images Vars
@@ -28,10 +28,11 @@ function App() {
     const contentP = content.children[1]
     const contentButton = content.children[2]
 
+    // Removes flash
     TweenMax.to(app, 0, {css: {visibility: 'visible'}})
 
     // Images Animation
-    tl.from(img1, 1.2, {y: 1280, ease: Power3.easeOut}, 'Start')
+    tl.from(img1, 1.2, {y: 1280, ease: Power3.easeOut}, 'HeroText')
       .from(img1.firstElementChild, 2, {scale: 1.6, ease: Power3.easeOut}, .2)
       .from(img2, 1.2, {y: 1280, ease: Power3.easeOut}, .4)
       .from(img2.firstElementChild, 2, {scale: 1.6, ease: Power3.easeOut}, .4)
@@ -41,9 +42,11 @@ function App() {
       y: 44,
       ease: Power3.easeOut,
       delay: .8
-    }, .15, 'Start')
+    }, .15, 'HeroText')
+    .from(contentP, 1, {y: 20, opacity: 0, ease: Power3.easeOut}, 1.4)
+    .from(contentButton, 1, {y: 20, opacity: 0, ease: Power3.easeOut}, 1.6)
 
-  })
+  }, [tl])
 
   return (
     <div className="hero" ref={el => app = el}>
@@ -56,10 +59,10 @@ function App() {
                   <div className="hero-content-line-inner">Hello,</div>
                 </div>
                 <div className="hero-content-line">
-                  <div className="hero-content-line-inner">I'm Alex Leybourne</div>
+                  <div className="hero-content-line-inner">I'm Alex Leybourne.</div>
                 </div>
                 <div className="hero-content-line">
-                  <div className="hero-content-line-inner">Full Stack dev</div>
+                  <div className="hero-content-line-inner">A Full Stack dev.</div>
                 </div>
               </h1>
               <p>I speak many languages including react, vue, ruby, html, scss and more.</p>
